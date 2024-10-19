2546a77533acb89600d47fe9b31bf88b14029e01
Mandaliya Urvish <mandaliyaurvishj7@gmail.com>
2024-10-19 17:08:35 +0530

    vue init

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
 import './bootstrap';
+import {createApp} from 'vue'
+import App from './src/App.vue'
+import router from '@/router/index.js'
+
+const app = createApp(App)
+app.use(router) 
+app.mount("#app")
--- routes/tenant.php
+++ routes/tenant.php
@@ -23,7 +23,7 @@ Route::middleware([
     InitializeTenancyByDomain::class,
     PreventAccessFromCentralDomains::class,
 ])->group(function () {
-    Route::get('/', function () {
+    Route::get('/tenant', function () {
         return 'This is your multi-tenant application. The id of the current tenant is ' . tenant('id');
     });
 });
